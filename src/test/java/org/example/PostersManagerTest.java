d26fb2c236e5294a5e7c9f3c216144a122cc73ce
Marina Shapovalova <snyzinka@gmail.com>
2023-04-21 08:49:04 +0300

    Initial commit

---
+++ src/test/java/org/example/PostersManagerTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PostersManagerTest {
+    @Test
+    public void shouldAddOneMovies() {
+        PostersManager manager = new PostersManager();
+
+        manager.addingMovie("Бладшот");
+
+
+        String[] actual = manager.findAll();
+        String[] expected = {"Бладшот"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddThreeMovies() {
+        PostersManager manager = new PostersManager();
+
+        manager.addingMovie("Бладшот");
+        manager.addingMovie("Вперед");
+        manager.addingMovie("Отель <<Белград>>");
+
+        String[] actual = manager.findAll();
+        String[] expected = {"Бладшот", "Вперед", "Отель <<Белград>>"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTheLatestAddedMovies() {
+        PostersManager manager = new PostersManager();
+
+        manager.addingMovie("Бладшот");
+        manager.addingMovie("Вперед");
+        manager.addingMovie("Отель <<Белград>>");
+        manager.addingMovie("Человек-невидимка");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Человек-невидимка", "Отель <<Белград>>", "Вперед", "Бладшот"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTheLatestAddMovies() {
+        PostersManager manager = new PostersManager(3);
+
+        manager.addingMovie("Бладшот");
+        manager.addingMovie("Вперед");
+        manager.addingMovie("Отель <<Белград>>");
+        manager.addingMovie("Человек-невидимка");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Человек-невидимка", "Отель <<Белград>>", "Вперед"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
